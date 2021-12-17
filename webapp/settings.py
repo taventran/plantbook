ccb7c1c6d8c537063b5965ec9042d6eb0c82118e
Taven Tran <taventran@gmail.com>
2021-12-16 23:08:25 -0600

    Hello

--- webapp/settings.py
+++ webapp/settings.py
@@ -160,7 +160,5 @@ DEFAULT_FILE_STORAGE = "plants.storages.CustomS3Boto3Storage"
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
-'''
-django_heroku.settings(locals())
 
-'''
+django_heroku.settings(locals())

3b9652dff0476d761760c76761d27f1c80a28bf0
Taven Tran <taventran@gmail.com>
2021-12-16 19:09:01 -0600

    Added django-heroku

--- webapp/settings.py
+++ webapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,3 +156,6 @@ AWS_S3_ADDRESSING_STYLE = 'virtual'
 
 DEFAULT_FILE_STORAGE = "plants.storages.CustomS3Boto3Storage"
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
+

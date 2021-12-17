f02bb08c8dbeb6c84e09f5dbde63b7a89a77d536
Taven Tran <taventran@gmail.com>
2021-12-16 18:33:53 -0600

    Allowed hosts change

--- webapp/settings.py
+++ webapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://django-plantbook.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['django-plantbook.herokuapp.com','127.0.0.1']
  
 
 # Application definition
